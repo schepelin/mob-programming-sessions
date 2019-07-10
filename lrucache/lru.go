431e876ae329b85cb45050fdedbf2c217a605ae1
Maxim Schepelin <mschepelin@ozon.ru>
2019-07-10 10:24:45 +0300

    Implemented simple cache with no lru

--- lrucache/lru.go
+++ lrucache/lru.go
@@ -1,1 +1,59 @@
 package lrucache
+
+// cacheItem represents a value stored in a cache
+type cacheItem struct {
+	value interface{}
+}
+
+// Cache describes a general interface of the cache
+type Cache interface {
+	Set(key string, value interface{}) (evicted bool)
+	Get(key string) (value interface{}, ok bool)
+	Has(key string) (ok bool)
+}
+
+// LRU implements the cache
+type LRU struct {
+	size     uint
+	capacity uint
+	storage  map[string]*cacheItem
+}
+
+// New is a factory function for LRU struct
+func New(capacity uint) *LRU {
+	return &LRU{
+		capacity: capacity,
+		storage:  make(map[string]*cacheItem),
+	}
+}
+
+// Set puts value into a cache
+func (lru *LRU) Set(key string, val interface{}) bool {
+	lru.storage[key] = &cacheItem{
+		value: val,
+	}
+	if lru.size > lru.capacity {
+		//TODO: run eviction here
+		return true
+	}
+	lru.size++
+	return false
+}
+
+// Has checks if a key in the cache
+func (lru *LRU) Has(key string) bool {
+	_, ok := lru.storage[key]
+	return ok
+}
+
+// Get returns value if it's presented in the cache
+func (lru *LRU) Get(key string) (interface{}, bool) {
+	item, ok := lru.storage[key]
+
+	if !ok {
+		return nil, false
+	}
+	// TODO: Update least recently used
+
+	return item.value, ok
+}
--- lrucache/lru_test.go
+++ lrucache/lru_test.go
@@ -1,3 +1,46 @@
 package lrucache
 
+import (
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_LRU_Set(t *testing.T) {
+
+	t.Run("Set stores item in an internal storage", func(t *testing.T) {
+		c := New(10)
+
+		evicted := c.Set("foo", "bar")
+
+		item := c.storage["foo"]
+		data, ok := item.value.(string)
+		assert.False(t, evicted)
+		assert.True(t, ok)
+		assert.Equal(t, "bar", data)
+	})
+
+}
+
+func TestLRU_Get(t *testing.T) {
+	t.Run("Get from empty cache", func(t *testing.T) {
+		c := New(1)
+
+		val, ok := c.Get("foo")
+
+		assert.False(t, ok)
+		assert.Nil(t, val)
+	})
+	t.Run("Get item is in cache", func(t *testing.T) {
+		c := New(1)
+		c.Set("foo", "bar")
+
+		item, ok := c.Get("foo")
+
+		val, _ := item.(string)
+		assert.True(t, ok)
+		assert.Equal(t, "bar", val)
+
+	})
+
+}
