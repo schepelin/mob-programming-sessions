83fd89ce239d2a5a2078ca212ff7375f50cd5483
Maxim Schepelin <mschepelin@ozon.ru>
2019-07-20 16:43:11 +0300

    After the session

--- lrucache/lru.go
+++ lrucache/lru.go
@@ -1,1 +1,85 @@
 package lrucache
+
+import (
+	"errors"
+)
+
+type CacheI interface {
+	Set(key string, value interface{}) (evicted bool)
+	Get(key string) (value interface{}, ok bool)
+	Has(key string) (ok bool)
+}
+
+type Cache struct {
+	values   map[string]*Node
+	capacity uint
+	head     *Node
+	tail     *Node
+}
+type Node struct {
+	value interface{}
+	prev  *Node
+	next  *Node
+}
+
+func NewCache(capacity uint) (*Cache, error) {
+	if capacity == 0 {
+		return nil, errors.New("Erorr! Cap 0")
+	}
+	return &Cache{
+		values:   make(map[string]*Node),
+		capacity: capacity,
+	}, nil
+}
+
+func (c *Cache) Has(key string) bool {
+	if _, ok := c.values[key]; ok {
+		return true
+	}
+	return false
+}
+
+func (c *Cache) Set(key string, value interface{}) bool {
+	n := &Node{value: value}
+	if c.head == nil {
+		c.head = n
+		c.tail = n
+	} else{
+		c.head.prev = n
+		n.next = c.head
+		c.head = n
+	}
+
+	_, ok := c.values[key]
+	if !ok && len(c.values) == int(c.capacity) {
+		// TODO: eviction
+	}
+	c.values[key] = n
+
+	return len(c.values) > int(c.capacity)
+}
+
+func (c *Cache) popTail() *Node{
+	if c.tail == nil{
+		return nil
+	}
+	n := c.tail
+	c.tail = c.tail.prev
+	if c.tail == nil{
+		c.head = nil
+	}
+	n.next = nil
+	n.prev = nil
+	return nil
+}
+
+func (c *Cache) Get(key string) (interface{}, bool) {
+	if v, ok := c.values[key]; ok {
+		return v.value, true
+	}
+	return nil, false
+}
+
+// func (c *Cache) Len() int {
+// 	return len(c.values)
+// }
--- lrucache/lru_test.go
+++ lrucache/lru_test.go
@@ -1,17 +1,144 @@
 package lrucache
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Cache_Methods(t *testing.T) {
+func Test_Cache_Empty_Cache_Return_False(t *testing.T) {
 	assert := assert.New(t)
-	t.Run("Empty cache's Has returns false", func(t *testing.T) {
-		c := NewCache(10)
+	t.Run("", func(t *testing.T) {
+		c, _ := NewCache(10)
 
 		assert.False(c.Has("unknown_key"))
 	})
+	t.Run("Test_Cache_First_El_Doesnt_Evicted", func(t *testing.T) {
+		c, _ := NewCache(10)
 
+		assert.False(c.Set("key", 1))
+	})
+	t.Run("Test_Cache_Cache_With_Zero_Cap_Not_Allow", func(t *testing.T) {
+		_, err := NewCache(0)
+		assert.Error(err)
+	})
+	t.Run("Same key doesn't increase sixe", func(t *testing.T) {
+		c, err := NewCache(10)
+		assert.NoError(err)
+		evicted := c.Set("key", 1)
+		assert.False(evicted)
+		evicted = c.Set("key", 1)
+		assert.False(evicted)
+	})
+}
+
+func TestCache_Has(t *testing.T) {
+	tests := []struct {
+		name     string
+		capacity uint
+		key      string
+		want     bool
+	}{
+		{
+			name:     "",
+			capacity: 1,
+			want:     false,
+		},
+		{
+			name:     "",
+			capacity: 1,
+			want:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, _ := NewCache(tt.capacity)
+			if got := c.Has(tt.key); got != tt.want {
+				t.Errorf("Cache.Has() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCache_Set(t *testing.T) {
+	tests := []struct {
+		name     string
+		capacity uint
+		key      string
+		value    interface{}
+		want     bool
+	}{
+		{
+			name:     "Empty cache's Set returns false",
+			key:      "a",
+			value:    1,
+			want:     false,
+			capacity: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, _ := NewCache(tt.capacity)
+			if got := c.Set(tt.key, tt.value); got != tt.want {
+				t.Errorf("Cache.Set() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCache_Set_ot_Seregi(t *testing.T) {
+	key := "razdva"
+	c, err := NewCache(1)
+	assert.NoError(t, err)
+	evicted := c.Set(key, 1)
+	assert.False(t, evicted)
+	assert.True(t, c.Has(key))
+}
+
+func TestCache_Get(t *testing.T) {
+	tests := []struct {
+		name     string
+		key      string
+		capacity uint
+		want     interface{}
+		want1    bool
+	}{
+		{
+			name: "Simple Get",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Cache{}
+			got, got1 := c.Get(tt.key)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Cache.Get() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("Cache.Get() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestCache_Get_ot_Pashi(t *testing.T) {
+	key := "razdva"
+	c, err := NewCache(1)
+	assert.NoError(t, err)
+	evicted := c.Set(key, 1)
+	assert.False(t, evicted)
+	val, ok := c.Get(key)
+	assert.True(t, ok)
+	assert.Equal(t, int(1), val)
+}
+
+func TestCache_Set_Eviction(t *testing.T) {
+	key := "razdva"
+	c, err := NewCache(1)
+	assert.NoError(t, err)
+	evicted := c.Set("keyevicted", 1)
+	assert.False(t, evicted)
+	evicted = c.Set(key, 1)
+	assert.True(t, evicted)
 }
